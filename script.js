41becd392b8d6d630f69c0daece40705da6e5ccd
Luigi <luigiagcaoili@gmail.com>
2021-03-10 22:35:55 -0500

    Add functionality for row buttons
    - Added `addR()` function
    - Added `removeR()` function
    - Added empty grid/table system
    - Added css example from class
    - Moved css to the same directory as js
    - Removed line breaks
    - Removed container styling

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
-let rows = 0
-let columns = 0
+let rows = 0;
+let columns = 0;
 let colors_selected;
 
-//adds rows to our grids 
+//adds rows to our grids
 function add_rows() {
-    alert("Row added")
+  const grid = document.querySelector("#grid");
+  // there are rows that can be removed
+  if (document.getElementsByTagName("tr").length > 0) {
+    let rows = document.getElementsByTagName("tr");
+    grid.appendChild(rows[0].cloneNode(true));
+  } else {
+    // there are no existing rows
+    let newRow = document.createElement("tr");
+    let rowData = document.createElement("td");
+    newRow.appendChild(rowData);
+    grid.children[0].appendChild(newRow);
+  }
 }
 
 function remove_rows() {
-    alert("Row removed")
+  // there are rows that can be removed
+  if (document.getElementsByTagName("tr").length > 0) {
+    let rows = document.getElementsByTagName("tr");
+    let lastRow = rows[rows.length - 1];
+    lastRow.parentElement.removeChild(lastRow);
+  }
 }
 
 function add_columns() {
-    alert("Column added")
+  alert("Column added");
 }
 
 function remove_columns() {
-    alert("Column removed")
+  alert("Column removed");
 }
 
 // select a color from a dropdown menu of colors
 function select_colors() {
-    //when a user selects a colors, notify 
-
+  //when a user selects a colors, notify
 }
 // click on a single cell, changing its color to the currently selected color
 function fill_cell() {
-    alert("cell colour changed")
+  alert("cell colour changed");
 }
 // fill all uncolored cells with the currently selected color
 function fill_all_uncolored() {
-    alert("filled uncolored cells with colours")
+  alert("filled uncolored cells with colours");
 }
 // fill all cells with the currently selected color
 function fill_all_cells() {
-    alert("filled all cells")
-
+  alert("filled all cells");
 }
 
 // clear all cells/restore all cells to their original/initial color
 function clear() {
-    alert("cells cleared")
+  alert("cells cleared");
 }
